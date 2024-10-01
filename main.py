e62e792568ef934f249b7bd72731d4cb7b4768e8
HonoratoOmar67 <omarhonorato03@gmail.com>
2024-09-30 23:52:42 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,160 @@
+#Jeremiah's Grade
+print(input("Enter Your Name:"))
+grade = ["47", "59", "93", "70", "89"]
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+
+totalGrade = (47 + 59 + 93 + 70 + 89)
+average = totalGrade / 5
+print("Your average is,", average)
+if average >= 90:
+    print("Letter Grade: A")
+elif average >= 80:
+    print("Letter Grade: B")
+elif average >= 70:
+    print("Letter Grade: C")
+elif average >= 60:
+    print("Letter Grade: D")
+else:
+    print("Letter Grade: F")
+############################
+#Torrance's Grade
+print(input("Enter Your Name:"))
+grade = ["94", "72", "91", "67", "100"]
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+
+totalGrade = (94 + 72 + 91 + 67 + 100)
+average = totalGrade / 5
+print("Your average is,", average)
+if average >= 90:
+    print("Letter Grade: A")
+elif average >= 80:
+    print("Letter Grade: B")
+elif average >= 70:
+    print("Letter Grade: C")
+elif average >= 60:
+    print("Letter Grade: D")
+else:
+    print("Letter Grade: F")
+#############################
+#Mary's Grade
+print(input("Enter Your Name:"))
+grade = ["92", "44", "94", "83", "79"]
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+
+totalGrade = (92 + 44 + 94 + 83 + 79)
+average = totalGrade / 5
+print("Your average is,", average)
+if average >= 90:
+    print("Letter Grade: A")
+elif average >= 80:
+    print("Letter Grade: B")
+elif average >= 70:
+    print("Letter Grade: C")
+elif average >= 60:
+    print("Letter Grade: D")
+else:
+    print("Letter Grade: F")
+############################
+#Beth's Grade
+print(input("Enter Your Name:"))
+grade = ["77", "32", "27", "100", "92"]
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+
+totalGrade = (77 + 32 + 27 + 100 + 92)
+average = totalGrade / 5
+print("Your average is,", average)
+if average >= 90:
+    print("Letter Grade: A")
+elif average >= 80:
+    print("Letter Grade: B")
+elif average >= 70:
+    print("Letter Grade: C")
+elif average >= 60:
+    print("Letter Grade: D")
+else:
+    print("Letter Grade: F")
+##############################
+#John's Grade
+print(input("Enter Your Name:"))
+grade = ["100", "100", "100", "99", "82"]
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+
+totalGrade = (100 + 100 + 100 + 99 + 82)
+average = totalGrade / 5
+print("Your average is,", average)
+if average >= 90:
+    print("Letter Grade: A")
+elif average >= 80:
+    print("Letter Grade: B")
+elif average >= 70:
+    print("Letter Grade: C")
+elif average >= 60:
+    print("Letter Grade: D")
+else:
+    print("Letter Grade: F")
+#############################
+#Larry's Grade from Impractical Jokers
+print(input("Enter Your Name:"))
+grade = ["44", "89", "77", "66", "100"]
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+
+totalGrade = (44 + 89 + 77 + 66 + 100)
+average = totalGrade / 5
+print("Your average is,", average)
+if average >= 90:
+    print("Letter Grade: A")
+elif average >= 80:
+    print("Letter Grade: B")
+elif average >= 70:
+    print("Letter Grade: C")
+elif average >= 60:
+    print("Letter Grade: D")
+else:
+    print("Letter Grade: F")
+#############################
+#Megan's Grade
+print(input("Enter Your Name:"))
+grade = ["50", "85", "75", "95", "95"]
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+print (input("Put in your grade: "))
+
+totalGrade = (50 + 85 + 75 + 95 + 95)
+average = totalGrade / 5
+print("Your average is,", average)
+if average >= 90:
+    print("Letter Grade: A")
+elif average >= 80:
+    print("Letter Grade: B")
+elif average >= 70:
+    print("Letter Grade: C")
+elif average >= 60:
+    print("Letter Grade: D")
+else:
+    print("Letter Grade: F")
